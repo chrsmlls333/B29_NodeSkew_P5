eb9d8b47bf0e6b9e273917fd8dd74164e4f98e8a
chrsmlls333 <chrsmlls333@gmail.com>
2021-10-04 00:29:37 -0500

    Custom init (using new CEM.js v0.2.1)

--- sketch.js
+++ sketch.js
@@ -1,7 +1,11 @@
-function setup() {
+import { shuffleArray } from "./libraries/cem/0.2.1/src/random.js";
+
+window.setup = () => {
   createCanvas(400, 400);
+  frameRate(1);
+  print(shuffleArray([1,2,3,4,5,6,7,8,9]));
 }
 
-function draw() {
-  background(220);
+window.draw = () => {
+  background(color(random(200,255), 0, 255)); 
 }
