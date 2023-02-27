bfbc12b0b52d37d3b6a849ca89419b34a296c96d
chrsmlls333 <chrsmlls333@gmail.com>
2023-02-27 01:19:37 -0600

    Rewrite Grid with math.gl vectors, better scaling and overflow

--- src/gestures/Grid.ts
+++ src/gestures/Grid.ts
@@ -1,96 +1,137 @@
-import { Vec, ceilVector, vecDivVec, vecMultScalar, vecMultVec, vecSubScalar } from "../libraries/cemjs/src/cem";
+import { Vector2 } from "@math.gl/core";
+import { cloneDeep } from "lodash";
+
+
 
 export interface GridNode {
-  index: Vec,
-  index0: Vec,
-  position: Vec
+  index: [ x: number, y: number ],
+  index0: [ x: number, y: number ],
+  position: [ x: number, y: number ]
 }
 
 export class Grid {
-  size = Vec(100, 100);
-  divs = 10;
-  cells = Vec(10, 10);
-  spacing = Vec(10, 10);
-  extra = 0;
+  size = new Vector2();
+  divs = 2;
+  numcells = new Vector2();
+  spacing = new Vector2();
+  extra = new Vector2();
+  squared = false
 
   nodeCache = {
     nodes: <GridNode[]>[],
     expired: true,
   }
 
-  constructor(_width = 100, _height = 100, _divisions = 10) {
-    this.updateGrid(_width, _height, _divisions)
+  #v = new Vector2()
+  static #blankNode: GridNode = {
+    index: [ 0, 0 ],
+    index0: [ 0, 0 ],
+    position: [ 0, 0 ]
   }
 
-  updateGrid(_width = this.size.x, _height = this.size.y, _divisions = this.divs, forceUpdate = false) {
+  constructor(_width = 100, _height = 100, _divisions = 10, _squared = false) {
+    this.updateGrid(_width, _height, _divisions, _squared)
+  }
+
+  updateGrid(
+    _width = this.size.x, 
+    _height = this.size.y, 
+    _divisions = this.divs, 
+    _squared = this.squared, 
+    forceUpdate = false
+  ) {
     if (
       !forceUpdate && 
       this.size && 
       _width === this.size.x && 
       _height === this.size.y && 
-      _divisions === this.divs
+      _divisions === this.divs && 
+      _squared === this.squared
     ) return
+    if (_width === 0 || _height === 0) return
 
-    this.size = Vec(_width, _height);
+    this.size.set(_width, _height);
     this.divs = _divisions;
+    this.squared = _squared;
 
     let minDimension = Math.min(this.size.x, this.size.y);
-    let numcells = vecMultScalar(this.size, this.divs/minDimension);
-    this.cells = ceilVector(numcells);
-    this.spacing = vecDivVec(this.size, this.cells);
-    this.extra = 0;
+    let numcells = this.size.clone().multiplyByScalar( this.divs / minDimension )
+    this.numcells.fromArray(numcells.map(n => Math.ceil(n)))
+    if (this.squared) {
+      let space = minDimension / Math.min(...this.numcells.toArray())
+      this.spacing.set(space, space)
+    } else {
+      this.spacing.copy(this.size).divide(this.numcells)
+    }
 
     this.nodeCache.expired = true;
   }
 
   adjustDivisions( divs: number = this.divs ) {
-    this.updateGrid(this.size.x, this.size.y, divs, true)
+    this.updateGrid(this.size.x, this.size.y, divs, this.squared, true)
     this.nodeCache.expired = true;
   }
 
-  adjustPadding( dist: number ) {
-    const minSpacingValue = Math.min(this.spacing.x, this.spacing.y);
-    const extraCellsPadding = Math.ceil((dist + 0.01) / minSpacingValue) + 1;
-    if (extraCellsPadding === this.extra) return
-    this.extra = extraCellsPadding;
+  
+  adjustPadding( _extrawidth = 0, _extraheight = 0, safety = 0.001) {
+    let absolutelyExtra = [_extrawidth, _extraheight].map(Math.abs)
+    let extraCells = this.#v
+      .fromArray(absolutelyExtra)
+      .divide(this.spacing)
+      .add([safety, safety])
+      .map(v => Math.ceil(v))
+    
+    if (this.extra.equals(extraCells)) return
+    this.extra.fromArray(extraCells);
     this.nodeCache.expired = true;
   }
 
+  
   getNodes( forceUpdate = false ) {
     if( forceUpdate || this.nodeCache.expired ) {
-      let nodes = <GridNode[]>[]
-      for (let i = 0; i < (this.cells.y + this.extra*2); i++) {
-        for (let j = 0; j < (this.cells.x + this.extra*2); j++) {
-          const index0 = Vec(j, i);
-          const index = vecSubScalar(index0, this.extra);
-          const position = vecMultVec(index, this.spacing);
-          const n = { index, index0, position }
-          nodes.push(n);
+      if (!this.nodeCache.nodes) this.nodeCache.nodes = []
+      let nodes = this.nodeCache.nodes
+      let xMax = (this.numcells.x + 1 + this.extra.x*2)
+      let yMax = (this.numcells.y + 1 + this.extra.y*2)
+      let len = xMax*yMax;
+
+      for (let y = 0; y < yMax; y++) {
+        for (let x = 0; x < xMax; x++) {
+          let i = x + (y * xMax)
+          if (!nodes[i]) nodes[i] = cloneDeep(Grid.#blankNode)
+          const node = this.nodeCache.nodes[i] 
+
+          this.#v.set(x, y).toArray(node.index0)
+          this.#v.subtract(this.extra).toArray(node.index)
+          this.#v.multiply(this.spacing).toArray(node.position)
         }
       }
-      this.nodeCache.nodes = nodes;
-      this.nodeCache.expired = false;
+      this.nodeCache.nodes.length = len
+      this.nodeCache.expired = false
     }
     return this.nodeCache.nodes;
   }
 
   getNodePosition( _x:number, _y:number, index0 = false ) {
     const nodes = this.getNodes();
-    let x = index0 ? _x : _x + this.extra;
-    let y = index0 ? _y : _y + this.extra;
-    return nodes[y * (this.cells.x + this.extra*2) + x].position;
+    let xMax = (this.numcells.x + 1 + this.extra.x*2)
+    let yMax = (this.numcells.y + 1 + this.extra.y*2)
+    let x = index0 ? _x : _x + this.extra.x;
+    let y = index0 ? _y : _y + this.extra.y;
+    return nodes[x + (y * xMax)].position;
   }
 
   get() {
-    const { size, cells, spacing, extra } = this;
+    const { size, numcells, spacing, extra, squared } = this;
     const nodes = this.getNodes();
     return {
       data: {
         size,
         cells: {
-          num: cells,
+          num: numcells,
           extra,
-          spacing
+          spacing,
+          squared
         }
       },
       nodes
